353bb73c18fb2b2c63f5fd585c4717aad6b586bd
Macarena Baltra <macarena.bc2011@gmail.com>
2017-06-29 10:39:35 -0400

    Regresando al caos

--- js/main.js
+++ js/main.js
@@ -1,68 +1,34 @@
-/*Guardar Datos*/
-
-function guardarDatos(){
-  localStorage.mensaje = document.getElementById('mensaje').value;
-  localStorage.mensajes2 = document.getElementById('mensajes2').value;
-}
-
-/*JQuery*/
 $(document).ready(function(){
-  $('.listaTarea').html(localStorage.getItem('listItems'));
-  $("button").click(function(e){ // no se como hacerlo sin el botón, pense en enter pero no lo logre
-    e.preventDefault();
-    var tarea = $('#toDoInput1').val();
-    var marcar = $("ul").append('<input type="checkbox>'); // no funciona u.u
+   $('button').click(function(e){ // no se como hacerlo sin el botón, pense en enter pero no lo logre
+     e.preventDefault();
+     var tarea = $('#toDoInput1').val();
+     var marcar = $('#mensaje').prop('checked', true); // no funciona u.u
 
-    if(tarea == ""){
-      alert("Debes escribir una tarea");
-    }else if{
-      $('.listaTarea').append("<li><input class='checkbox' type='checkbox'/>" + tarea + "<a class='remove'>x</a><hr></li>");
-      localStorage.setItem('listItems', $('.listaTarea').html());
-      $('#toDoInput1').val("");
-    }
-  });
-  
-  $(document).on('change', '.checkbox', function()
-    {
-      if($(this).attr('checked'))
-      {
-        $(this).removeAttr('checked');
-      }
-      else
-      {
-        $(this).attr('checked', 'checked');
-      }
+     if(tarea == ""){
+       alert("Debes escribir una tarea");
+     }else {
+     $('#mensaje').append('<div><span class="text-left"><p>' /*+ marcar*/ + tarea + '<button>Remove</button></p></span></div>');
+     }
+   });
+});
 
-      $(this).parent().toggleClass('completed');
-       
-      localStorage.setItem('listItems', $('.listaTarea').html());
-    });
-   
-    $(document).on('click', '.remove', function()
-    {
-      $(this).parent().remove();
-      localStorage.setItem('listItems', $('.listaTarea').html());
-    });
-  });
-
-
-
-  $("button").click(function(e){ // no se como hacerlo sin el botón, pense en enter pero no lo logre
+ 
+$(document).ready(function(){
+   $("button").click(function(e){ // no se como hacerlo sin el botón, pense en enter pero no lo logre
     e.preventDefault();
     var tarea1 = $('#toDoInput1').val();
-    var marcar1 = $("ul").append('<input type="checkbox>'); // no funciona u.u
+    var marcar1 = $('#mensajes2').prop("checked",true);
     // No funciona lo /**/
-  /*  var caja = [];
+    /*var caja = [];
 
     $("#toDoInput1 input[type=checkbox]:checked").each(
     function ()
     {
         caja.push($(this).data("mensajes2"));
     });
-  */
-    if(tarea == checked){
-      $('#mensajes2').append('<div><span class="text-left">' + tarea + '<button>Remove</button></p></span></div>');
-    }
+    */
+     if(tarea == checked){
+       $('#mensajes2').append('<div><span class="text-left">' + tarea + '<button>Remove</button></p></span></div>');
+     }
   });
-
 });
