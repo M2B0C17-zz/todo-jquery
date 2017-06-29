a656f8cde584c1a527db8aa6cea9259c566b0969
Macarena Baltra <macarena.bc2011@gmail.com>
2017-06-29 09:22:41 -0400

    Intentos de checbox + separción de ules

--- js/main.js
+++ js/main.js
@@ -10,10 +10,21 @@ $(document).ready(function(){
   $("button").click(function(e){ // no se como hacerlo sin el botón, pense en enter pero no lo logre
     e.preventDefault();
     var tarea = $('#toDoInput1').val();
+    var marcar = $("#mensaje").prop("checked"); // no funciona
     if(tarea == ""){
       alert("Debes escribir una tarea");
     }else {
       $('#mensaje').append('<div><span class="text-left"><p>' + tarea + '<button>Remove</button></p></span></div>');
+    }
+  });
+});
+
+$(document).ready(function(){
+  $("button").click(function(e){ // no se como hacerlo sin el botón, pense en enter pero no lo logre
+    e.preventDefault();
+    var tarea = $('#toDoInput1').val();
+    var marcar = $("#mensajes2").prop("checked");
+    if(tarea == checked){
       $('#mensajes2').append('<div><span class="text-left">' + tarea + '<button>Remove</button></p></span></div>');
     }
   });
