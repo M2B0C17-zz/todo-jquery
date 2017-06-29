24a7da73607160082ab7c6655858f5660a502a44
Macarena Baltra <macarena.bc2011@gmail.com>
2017-06-29 10:23:58 -0400

    Intentos de checbox 5 ... creo

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ function guardarDatos(){
 
 /*JQuery*/
 $(document).ready(function(){
+  $('.listaTarea').html(localStorage.getItem('listItems'));
   $("button").click(function(e){ // no se como hacerlo sin el botón, pense en enter pero no lo logre
     e.preventDefault();
     var tarea = $('#toDoInput1').val();
@@ -14,13 +15,38 @@ $(document).ready(function(){
 
     if(tarea == ""){
       alert("Debes escribir una tarea");
-    }else {
-      $('#mensaje').append('<div><span class="text-left"><p>' /*+ marcar*/ + tarea + '<button>Remove</button></p></span></div>');
+    }else if{
+      $('.listaTarea').append("<li><input class='checkbox' type='checkbox'/>" + tarea + "<a class='remove'>x</a><hr></li>");
+      localStorage.setItem('listItems', $('.listaTarea').html());
+      $('#toDoInput1').val("");
     }
   });
-});
+  
+  $(document).on('change', '.checkbox', function()
+    {
+      if($(this).attr('checked'))
+      {
+        $(this).removeAttr('checked');
+      }
+      else
+      {
+        $(this).attr('checked', 'checked');
+      }
 
-$(document).ready(function(){
+      $(this).parent().toggleClass('completed');
+       
+      localStorage.setItem('listItems', $('.listaTarea').html());
+    });
+   
+    $(document).on('click', '.remove', function()
+    {
+      $(this).parent().remove();
+      localStorage.setItem('listItems', $('.listaTarea').html());
+    });
+  });
+
+
+
   $("button").click(function(e){ // no se como hacerlo sin el botón, pense en enter pero no lo logre
     e.preventDefault();
     var tarea1 = $('#toDoInput1').val();
@@ -38,4 +64,5 @@ $(document).ready(function(){
       $('#mensajes2').append('<div><span class="text-left">' + tarea + '<button>Remove</button></p></span></div>');
     }
   });
+
 });
