5a49d50c90ddbedf37e317fabc9d68011e57eaf5
Macarena Baltra <macarena.bc2011@gmail.com>
2017-06-29 10:56:11 -0400

    Código Roto

--- js/main.js
+++ js/main.js
@@ -1,16 +1,39 @@
 $(document).ready(function(){
-   $('button').click(function(e){ // no se como hacerlo sin el botón, pense en enter pero no lo logre
-     e.preventDefault();
-     var tarea = $('#toDoInput1').val();
-     var marcar = $('#mensaje').prop('checked', true); // no funciona u.u
+  var tarea = $('input'),
+  var btn = $('#btnAgregar'),
+  var ul = $('#mensaje'),
+  var rmvBtn = $('.rmvBtn');
 
-     if(tarea == ""){
-       alert("Debes escribir una tarea");
-     }else {
-     $('#mensaje').append('<div><span class="text-left"><p>' /*+ marcar*/ + tarea + '<button>Remove</button></p></span></div>');
-     }
-   });
+
+  btn.on('click', function(e) {
+  e.preventDefault();
+
+  if(tarea == ""){
+    alert("Debes escribir una tarea");
+  } else {
+    $('·mensaje').prepend('<li><input type="checkbox" class="check"> ' + tarea.val() + ' <button class="rmvBtn">x</button></li>');
+    tarea.val('');
+    }
+  });   
+
+  function remove() {
+    $(this).closest('li').remove();
+  }
+
+  function marcado() {
+    if (this.checked) {
+      $(this).closest('li').addClass('done');
+    } else {
+      $(this).closest('li').removeClass('done');
+    }
+  }
+
+  $(document).on('click', '.listaTarea', markThis);
+
+  $(document).on('click', '.rmvBtn', removeItem);
+   
 });
+
 
  
 $(document).ready(function(){
